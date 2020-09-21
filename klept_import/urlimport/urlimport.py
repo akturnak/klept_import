c597571c161c61ffbbc9835c3b811014553647ef
akturnak <abdulin.mirat@gmail.com>
2020-09-21 09:53:19 +0300

    add UrlModuleLoader

--- klept_import/urlimport/urlimport.py
+++ klept_import/urlimport/urlimport.py
@@ -84,3 +84,54 @@ class UrlMetaFinder(importlib.abc.MetaPathFinder):
         def invalidate_caches(self):
             log.debug("invalidating link cache")
             self._links.clear()
+
+
+# Module Loader for a URL
+class UrlModuleLoader(importlib.abc.SourceLoader):
+    def __init__(self, baseurl):
+        self._baseurl = baseurl
+        self._source_cache = {}
+
+    def module_repr(self, module):
+        return "<urlmodule %r from %r>" % (module.__name__, module.__file__)
+
+    # Required method
+    def load_module(self, fullname):
+        code = self.get_code(fullname)
+        mod = sys.modules.setdefault(fullname, imp.new_module(fullname))
+        mod.__file__ = self.get_filename(fullname)
+        mod.__loader__ = self
+        mod.__package__ = fullname.rpartition(".")[0]
+        exec(code, mod.__dict__)
+
+        return mod
+
+    # Optional extensions
+    def get_code(self, fullname):
+        src = self.get_source(fullname)
+        return compile(src, self.get_filename(fullname), "exec")
+
+    def get_data(self, path):
+        pass
+
+    def get_filename(self, fullname):
+        return self._baseurl + "/" + fullname.split(".")[-1] + ".py"
+
+    def get_source(self, fullname):
+        filename = self.get_filename(fullname)
+        log.debug("loader: %r loaded", filename)
+        if filename in self._source_cache:
+            log.debug("loader: cached: %r", filename)
+            return self._source_cache[filename]
+        try:
+            u = urlopen(filename)
+            source = u.read().decode("utf-8")
+            log.debug("loader: %r loaded", filename)
+            self._source_cache[filename] = source
+            return source
+        except (HTTPError, URLError) as e:
+            log.debug("loaded: %r failed. %s", filename, e)
+            raise ImportError("Can't load %s" % filename)
+
+    def is_package(self, fullname):
+        return False
